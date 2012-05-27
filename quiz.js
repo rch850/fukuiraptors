bc316e58eb3f92d1da27fe0aec3a62b82b00862d
rch850 <rich850@gmail.com>
2012-05-27 17:11:44 +0900

    外れたときに音を出す用にしました

--- quiz.js
+++ quiz.js
@@ -5,6 +5,13 @@
 //
 
 $(function() {
+  $("a[href='ans-wrong.html']").on("click", function() {
+    // http://www.dinosaurfact.net/Sounds.php
+    var audio = new Audio("");
+    audio.src = "wav/Trex31.wav";
+    audio.load();
+    audio.play();
+  });
   $("a[href='ans-right.html']").on("click", function() {
     var quiz = location.pathname.substring(location.pathname.lastIndexOf("q"));
     dino100.setSolved(quiz);
