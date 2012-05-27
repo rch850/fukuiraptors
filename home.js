295032beb78c5726d95a07247df37e639ea72839
rch850 <rich850@gmail.com>
2012-05-27 11:51:45 +0900

    福井県かどうかを判別して、レベルの class を切り替えるようにしました。

---
+++ home.js
@@ -0,0 +1,40 @@
+// ホーム画面用 JavaScript
+//
+// 参考ドキュメント
+// - Geolocation: https://developer.mozilla.org/ja/Using_geolocation
+// - 逆ジオコーディング: https://developers.google.com/maps/documentation/javascript/services?hl=ja#ReverseGeocoding
+
+$(function() {
+  /**
+   * @return 逆ジオコーディングの結果が福井県かどうか
+   */
+  function isGeocodeResultFukuiPrefecture(results) {
+    var result = false;
+    $.each(results, function() {
+      $.each(this.address_components, function() {
+        if (this.long_name === "Fukui Prefecture") {
+          result = true;
+          return false;
+        }
+      });
+      if (result) { return false; }
+    });
+    return result;
+  }
+
+  var geocoder = new google.maps.Geocoder();
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latlng = new google.maps.LatLng(
+      position.coords.latitude,
+      position.coords.longitude);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status !== google.maps.GeocoderStatus.OK) {
+        console.info("status is " + status);
+        return;
+      }
+      if (isGeocodeResultFukuiPrefecture(results)) {
+        $('li.level:nth-child(2)').removeClass('disable');
+      }
+    });
+  });
+});
