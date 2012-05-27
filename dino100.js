c469d9b95544b8356afbcce70aac21c3cc418433
rch850 <rich850@gmail.com>
2012-05-27 15:41:13 +0900

    ランダムな問題がでるようにしました

--- dino100.js
+++ dino100.js
@@ -4,16 +4,41 @@
 
 var dino100 = dino100 || {};
 (function() {
+  var LV1 = ["q001.html", "q002.html", "q003.html"];
+  var LV2 = ["q004.html", "q005.html", "q006.html"];
+  var LV3 = ["q007.html", "q008.html", "q009.html"];
+
+  // 初期状態のオブジェクトを入れておく。
+  if (!localStorage.dino100) {
+    localStorage.dino100 = JSON.stringify({
+      solved: [],
+      recentQuiz: "",
+    });
+  }
+
+  function getRandomElement(ary) {
+    var obj = JSON.parse(localStorage.dino100);
+    var elem = ary[Math.floor(Math.random() * ary.length)];
+    while (elem === obj.recentQuiz) {
+      elem = ary[Math.floor(Math.random() * ary.length)];
+    }
+    return elem;
+  }
+
   dino100.getQuizLevel1 = function() {
-    return "q001.html";
+    var obj = JSON.parse(localStorage.dino100);
+    if (obj.solved.length === 0) {
+      return "q001.html";
+    }
+    return getRandomElement(LV1);
   }
 
   dino100.getQuizLevel2 = function() {
-    return "q002.html";
+    return getRandomElement(LV2);
   }
 
   dino100.getQuizLevel3 = function() {
-    return "q003.html";
+    return getRandomElement(LV3);
   }
 })();
 
